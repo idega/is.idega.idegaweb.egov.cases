cc892bdb7ad8797b3603105c8fb9491cf01c2e35
Pall Helgason <palli@idega.com>
2007-03-16 13:26:48 +0000

    Moved the biztalk and onesystem stuff out of the cases bundle and into is.idega.idegaweb.egov.cases.onesystem and is.idega.idegaweb.egov.cases.biztalk.

--- src/java/is/idega/idegaweb/egov/cases/IWBundleStarter.java
+++ src/java/is/idega/idegaweb/egov/cases/IWBundleStarter.java
@@ -9,15 +9,10 @@
  */
 package is.idega.idegaweb.egov.cases;
 
-import is.idega.idegaweb.egov.cases.business.BizTalkEventListener;
 import is.idega.idegaweb.egov.cases.business.CasesBusiness;
-import is.idega.idegaweb.egov.cases.business.OneSystemEventListener;
 import is.idega.idegaweb.egov.cases.util.CaseConstants;
 
-import com.idega.block.process.business.CaseBusiness;
 import com.idega.block.process.business.CaseCodeManager;
-import com.idega.business.IBOLookup;
-import com.idega.idegaweb.IWApplicationContext;
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWBundleStartable;
 import com.idega.idegaweb.include.GlobalIncludeManager;
@@ -35,29 +30,5 @@ public class IWBundleStarter implements IWBundleStartable {
 	}
 	
 	private void registerCaseChangeListener(IWBundle starterBundle) {
-		IWApplicationContext iwac = starterBundle.getApplication().getIWApplicationContext();
-		String sendToOneSystem = iwac.getApplicationSettings().getProperty("WS_SEND_ONE_SYSTEM", "false");
-		String sendToBizTalk = iwac.getApplicationSettings().getProperty("WS_SEND_BIZ_TALK", "false");
-		CaseBusiness caseBusiness;
-		try {
-			caseBusiness = (CaseBusiness)IBOLookup.getServiceInstance(iwac,CaseBusiness.class);
-			//String choiceCaseCode = SchoolConstants.SCHOOL_CHOICE_CASE_CODE_KEY;
-			//registering the event listener on when the schoolchoiceapplication gets status placed
-			//@TODO fix the codes
-			if (!sendToOneSystem.equals("false")) {
-				System.out.println("Adding one system listeners");
-				caseBusiness.addCaseChangeListener(new OneSystemEventListener(), CaseConstants.CASE_CODE_KEY);
-				caseBusiness.addCaseChangeListener(new OneSystemEventListener(), "MBANBOP");
-			}
-			if (!sendToBizTalk.equals("false")) {
-				System.out.println("Adding biz talk listener");
-				caseBusiness.addCaseChangeListener(new BizTalkEventListener(), CaseConstants.CASE_CODE_KEY);
-			}
-		}
-		catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
-
 }
