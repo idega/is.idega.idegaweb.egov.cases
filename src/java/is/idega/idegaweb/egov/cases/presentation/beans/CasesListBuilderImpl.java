08796c2a1acefe93970a56609a6a9ea6590e8928
Valdas Zemaitis <valdas@idega.com>
2008-10-13 10:25:20 +0000

    *** empty log message ***

--- src/java/is/idega/idegaweb/egov/cases/presentation/beans/CasesListBuilderImpl.java
+++ src/java/is/idega/idegaweb/egov/cases/presentation/beans/CasesListBuilderImpl.java
@@ -633,7 +633,7 @@ public class CasesListBuilderImpl implements GeneralCasesListBuilder {
 		scripts.add(CoreConstants.DWR_ENGINE_SCRIPT);
 		scripts.add(CoreConstants.DWR_UTIL_SCRIPT);
 		scripts.add("/dwr/interface/CasesEngine.js");
-//		scripts.add("/idegaweb/bundles/is.idega.idegaweb.egov.bpm.bundle/resources/javascript/CasesBPMAssets.js");
+//		scripts.add("/idegaweb/bundles/is.idega.idegaweb.egov.bpm/resources/javascript/CasesBPMAssets.js");
 		
 		if (caseId == null || CoreConstants.EMPTY.equals(caseId)) {
 			caseId = iwc.getParameter(CasesProcessor.PARAMETER_CASE_PK + "_id");
