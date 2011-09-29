7b4b34c7f4ee166f0b6fbad1a0f23dc917796074
Valdas Žemaitis <valdas@idega.com>
2011-09-29 15:17:30 +0300

    Determining if advanced input search box should be used when searching by BPM process variable

--- resources/javascript/CasesListHelper.js
+++ resources/javascript/CasesListHelper.js
@@ -727,26 +727,39 @@ CasesListHelper.addVariableInput = function() {
 	var isMultipleObjectsField = optionValue[1] == 'B';
 	var isHandlerVar = optionValue[0] == 'handlerUserId';
 	var procDefId = dwr.util.getValue(jQuery('select.availableVariablesChooserForProcess').attr('id'));
-	var options = {
-		className: isDateField ? 'com.idega.presentation.ui.IWDatePicker' : isMultipleObjectsField || isHandlerVar ? 'com.idega.presentation.ui.SelectionBox' : 'com.idega.presentation.ui.TextInput',
-		properties: [
-						{id: 'setId', value: id},
-						{id: 'setStyleClass', value: 'variableValueField'},
-						{id: isDateField ? 'setInputName' : 'setName', value: dwr.util.getValue(chooserId)},
-						{id: 'setValue', value: isMultipleObjectsField || isHandlerVar ?
-													'#{bpmVariableValueResolver' + optionValue[0] + '.getValues(\'' + procDefId + '\', \'' + optionValue[0] + '\')}' :
-													null},
-						{id: 'setOnKeyUp', value: 'if (isEnterEvent(event)) { CasesListHelper.addVariablesAndSearch(); } return false;'}
-					],
-		container: id2,
-		callback: function() {
-			closeAllLoadingMessages();
-			dwr.util.setValue(chooserId, '-1');
-			jQuery('#' + id2).append('<img class=\'variableFieldDeleter\' onclick="jQuery(\'#' + id2 + '\').hide(\'normal\', function() {jQuery(\'#' + id2 + '\').remove();});" src=\''+deleteImage+'\' />').show('fast');
-		},
-		append: true
-	};
-	IWCORE.getRenderedComponentByClassName(options);
+	LazyLoader.loadMultiple(['/dwr/engine.js', '/dwr/interface/WebUtil.js'], function() {
+		WebUtil.getBooleanApplicationProperty("advanced_bpm_cases_search", true, {
+			callback: function(result) {
+				var options = {
+					className: isDateField ? 'com.idega.presentation.ui.IWDatePicker' : 'com.idega.presentation.ui.TextInput',
+					properties: [
+									{id: 'setId', value: id},
+									{id: 'setStyleClass', value: 'variableValueField'},
+									{id: isDateField ? 'setInputName' : 'setName', value: dwr.util.getValue(chooserId)},
+									{id: 'setOnKeyUp', value: 'if (isEnterEvent(event)) { CasesListHelper.addVariablesAndSearch(); } return false;'}
+								],
+					container: id2,
+					callback: function() {
+						closeAllLoadingMessages();
+						dwr.util.setValue(chooserId, '-1');
+						jQuery('#' + id2).append('<img class=\'variableFieldDeleter\' onclick="jQuery(\'#' + id2 + '\').hide(\'normal\', function() {jQuery(\'#' + id2 + '\').remove();});" src=\''+deleteImage+'\' />').show('fast');
+					},
+					append: true
+				};
+				var valueToSet = null;
+				if (isHandlerVar) {
+					options.className = 'com.idega.presentation.ui.DropdownMenu';
+					valueToSet = '#{bpmVariableValueResolver' + optionValue[0] + '.getValues(\'' + procDefId + '\', \'' + optionValue[0] + '\')}';
+				} else if (isMultipleObjectsField && result) {
+					options.className = 'com.idega.presentation.ui.SelectionBox';
+					valueToSet = '#{bpmVariableValueResolver' + optionValue[0] + '.getValues(\'' + procDefId + '\', \'' + optionValue[0] + '\')}';
+				}
+				if (valueToSet != null)
+					options.properties.push({id: 'setValue', value: valueToSet});
+				IWCORE.getRenderedComponentByClassName(options);
+			}
+		});
+	}, null);	
 }
 
 CasesListHelper.addVariablesAndSearch = function() {
