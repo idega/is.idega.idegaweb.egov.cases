08cb2fd64332a03a3fa1214d6bd2cf88dc29e74a
Valdas Žemaitis <valdas@idega.com>
2012-04-05 10:59:43 +0300

    Fixed cases board exporter - now it supports financing table

--- src/java/is/idega/idegaweb/egov/cases/media/CasesBoardViewerExporter.java
+++ src/java/is/idega/idegaweb/egov/cases/media/CasesBoardViewerExporter.java
@@ -10,6 +10,9 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
@@ -23,6 +26,7 @@ import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.idega.block.process.business.ProcessConstants;
@@ -38,6 +42,7 @@ import com.idega.user.data.User;
 import com.idega.util.CoreConstants;
 import com.idega.util.FileUtil;
 import com.idega.util.IOUtil;
+import com.idega.util.ListUtil;
 import com.idega.util.StringHandler;
 import com.idega.util.expression.ELUtil;
 
@@ -62,9 +67,8 @@ private MemoryFileBuffer memory;
 		String uuid = iwc.getParameter(CasesBoardViewer.PARAMETER_UUID);
 
 		CaseBoardTableBean data = boardCasesManager.getTableData(iwc, caseStatus, processName, uuid);
-		if (data == null || !data.isFilledWithData()) {
+		if (data == null || !data.isFilledWithData())
 			return;
-		}
 
 		IWResourceBundle iwrb = iwc.getIWMainApplication().getBundle(CasesConstants.IW_BUNDLE_IDENTIFIER).getResourceBundle(iwc);
 
@@ -78,37 +82,115 @@ private MemoryFileBuffer memory;
 		HSSFCellStyle bigStyle = workBook.createCellStyle();
 		bigStyle.setFont(bigFont);
 
-		HSSFSheet sheet = workBook.createSheet(StringHandler.shortenToLength(iwrb.getLocalizedString("cases_board_viewer.cases_board", "Cases board"), 30));
+		HSSFSheet sheet = workBook.createSheet(StringHandler.shortenToLength(iwrb.getLocalizedString("cases_board_viewer.cases_board",
+				"Cases board"), 30));
 		int rowNumber = 0;
-		createHeaders(sheet, bigStyle, data.getHeaderLabels(), rowNumber);
-		rowNumber++;
+		createHeaders(sheet, bigStyle, data.getHeaderLabels(), rowNumber++);
 		for (CaseBoardTableBodyRowBean rowBean: data.getBodyBeans()) {
-			HSSFRow row = sheet.createRow(rowNumber);
+			HSSFRow row = sheet.createRow(rowNumber++);
 
-			short index = 0;
+			int cellIndex = 0;
 			Map<Integer, List<AdvancedProperty>> values = rowBean.getValues();
 			for (Integer key: values.keySet()) {
-				HSSFCell bodyRowCell = row.createCell(index);
-				List<AdvancedProperty> entries = values.get(key);
-				for (AdvancedProperty entry: entries) {
-					if (boardCasesManager.isColumnOfDomain(entry.getId(), CasesBoardViewer.CASE_FIELDS.get(5).getId())) {
-						bodyRowCell.setCellValue(rowBean.getCaseIdentifier());
-					} else if (boardCasesManager.isColumnOfDomain(entry.getId(), ProcessConstants.HANDLER_IDENTIFIER)) {
-						bodyRowCell.setCellValue(getHandlerInfo(iwc, rowBean.getHandler()));
-					} else if (boardCasesManager.isColumnOfDomain(entry.getId(), CasesBoardViewer.CASE_FIELDS.get(13).getId())) {
-						System.out.println(getClass().getName() + ": add table!");	//	TODO
-					} else {
-						bodyRowCell.setCellValue(entry.getValue());
+				if (key < CasesBoardViewer.CASE_FIELDS.size() &&
+						ProcessConstants.FINANCING_OF_THE_TASKS.equals(CasesBoardViewer.CASE_FIELDS.get(key).getId())) {
+					//	Financing table
+					List<Map<String, String>> financingInfo = rowBean.getFinancingInfo();
+					if (ListUtil.isEmpty(financingInfo)) {
+						financingInfo = new ArrayList<Map<String,String>>();
+						Map<String, String> emptyValues = new HashMap<String, String>();
+						emptyValues.put(CasesBoardViewer.WORK_ITEM, CoreConstants.MINUS);
+						emptyValues.put(CasesBoardViewer.ESTIMATED_COST, CoreConstants.MINUS);
+						emptyValues.put(CasesBoardViewer.BOARD_SUGGESTION, CoreConstants.MINUS);
+						emptyValues.put(CasesBoardViewer.BOARD_DECISION, CoreConstants.MINUS);
+						financingInfo.add(emptyValues);
+						rowBean.setFinancingInfo(financingInfo);
 					}
+
+					cellIndex += 4;
+					long estimationTotal = 0;
+					long suggestionTotal = 0;
+					long decisionTotal = 0;
+					HSSFRow financingTableRow = row;
+					for (Iterator<Map<String, String>> infoIter = financingInfo.iterator(); infoIter.hasNext();) {
+						Map<String, String> info = infoIter.next();
+						int financingTableCellIndex = key;
+
+						HSSFCell cell = financingTableRow.createCell(financingTableCellIndex++, HSSFCell.CELL_TYPE_STRING);
+						cell.setCellValue(info.get(CasesBoardViewer.WORK_ITEM));
+
+						cell = financingTableRow.createCell(financingTableCellIndex++, HSSFCell.CELL_TYPE_NUMERIC);
+						String estimation = info.get(CasesBoardViewer.ESTIMATED_COST);
+						estimationTotal += boardCasesManager.getNumberValue(estimation);
+						cell.setCellValue(estimation);
+
+						cell = financingTableRow.createCell(financingTableCellIndex++, HSSFCell.CELL_TYPE_NUMERIC);
+						String suggestion = info.get(CasesBoardViewer.BOARD_SUGGESTION);
+						long sugg = boardCasesManager.getNumberValue(suggestion);
+						suggestionTotal += sugg;
+						cell.setCellValue(String.valueOf(sugg));
+
+						cell = financingTableRow.createCell(financingTableCellIndex, HSSFCell.CELL_TYPE_NUMERIC);
+						String decision = info.get(CasesBoardViewer.BOARD_DECISION);
+						long dec = boardCasesManager.getNumberValue(decision);
+						decisionTotal += dec;
+						cell.setCellValue(String.valueOf(dec));
+
+						if (infoIter.hasNext())
+							financingTableRow = sheet.createRow(rowNumber++);
+					}
+
+					//	Empty row
+					financingTableRow = sheet.createRow(rowNumber++);
+
+					//	Totals
+					financingTableRow = sheet.createRow(rowNumber++);
+
+					int financingTableCellIndex = key;
+
+					HSSFCell cell = financingTableRow.createCell(financingTableCellIndex++, HSSFCell.CELL_TYPE_STRING);
+					cell.setCellValue(iwrb.getLocalizedString("total", "Total"));
+
+					cell = financingTableRow.createCell(financingTableCellIndex++, HSSFCell.CELL_TYPE_NUMERIC);
+					cell.setCellValue(String.valueOf(estimationTotal));
+
+					cell = financingTableRow.createCell(financingTableCellIndex++, HSSFCell.CELL_TYPE_NUMERIC);
+					cell.setCellValue(String.valueOf(suggestionTotal));
+
+					cell = financingTableRow.createCell(financingTableCellIndex++, HSSFCell.CELL_TYPE_NUMERIC);
+					cell.setCellValue((String.valueOf(decisionTotal)));
+				} else {
+					//	Simple values
+					HSSFCell bodyRowCell = row.createCell(cellIndex);
+					List<AdvancedProperty> entries = values.get(key);
+					for (AdvancedProperty entry: entries) {
+						if (boardCasesManager.isColumnOfDomain(entry.getId(), CasesBoardViewer.CASE_FIELDS.get(5).getId())) {
+							bodyRowCell.setCellValue(rowBean.getCaseIdentifier());
+						} else if (boardCasesManager.isColumnOfDomain(entry.getId(), ProcessConstants.HANDLER_IDENTIFIER)) {
+							bodyRowCell.setCellValue(getHandlerInfo(iwc, rowBean.getHandler()));
+						} else {
+							bodyRowCell.setCellValue(entry.getValue());
+						}
+					}
+
+					cellIndex++;
 				}
-
-				index++;
 			}
-
-			rowNumber++;
 		}
 
 		createHeaders(sheet, null, data.getFooterValues(), rowNumber);
+
+		for (int i = 0; i < rowNumber; i++) {
+			HSSFRow row = sheet.getRow(i);
+			int cell = 0;
+			for (Iterator<Cell> cellsIter = row.cellIterator(); cellsIter.hasNext();) {
+				cellsIter.next();
+
+				if (row.getCell(cell) != null)
+					sheet.autoSizeColumn(cell);
+				cell++;
+			}
+		}
 
 		try {
 			workBook.write(streamOut);
@@ -120,8 +202,8 @@ private MemoryFileBuffer memory;
 		}
 
 		memory.setMimeType(MimeTypeUtil.MIME_TYPE_EXCEL_2);
-		setAsDownload(iwc, new StringBuilder(iwrb.getLocalizedString("cases_board_viewer.exported_data", "Exported cases board")).append(".xls").toString(),
-				memory.length());
+		setAsDownload(iwc, new StringBuilder(iwrb.getLocalizedString("cases_board_viewer.exported_data", "Exported cases board")).append(".xls")
+				.toString(), memory.length());
 	}
 
 	private String getHandlerInfo(IWContext iwc, User handler) {
@@ -142,7 +224,7 @@ private MemoryFileBuffer memory;
 	private void createHeaders(HSSFSheet sheet, HSSFCellStyle cellStyle, List<String> labels, int rowNumber) {
 		HSSFRow row = sheet.createRow(rowNumber);
 
-		short cellIndex = 0;
+		int cellIndex = 0;
 		for (String label: labels) {
 			HSSFCell cell = row.createCell(cellIndex++);
 			cell.setCellValue(label);
@@ -155,7 +237,7 @@ private MemoryFileBuffer memory;
 	private void createHeaders(HSSFSheet sheet, HSSFCellStyle cellStyle, Map<Integer, List<AdvancedProperty>> headers, int rowNumber) {
 		HSSFRow row = sheet.createRow(rowNumber);
 
-		short cellIndex = 0;
+		int cellIndex = 0;
 		for (Integer key: headers.keySet()) {
 			List<AdvancedProperty> labels = headers.get(key);
 			for (AdvancedProperty label: labels) {
