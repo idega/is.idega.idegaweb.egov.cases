a39d55c2db923f6e55be9ef8d93b4f095d82f984
Valdas Žemaitis <valdas@idega.com>
2021-07-20 14:30:32 +0300

    Using Excel util

--- src/java/is/idega/idegaweb/egov/cases/media/impl/ExcelExporterServiceImpl.java
+++ src/java/is/idega/idegaweb/egov/cases/media/impl/ExcelExporterServiceImpl.java
@@ -1,13 +1,11 @@
 package is.idega.idegaweb.egov.cases.media.impl;
 
-import java.util.Iterator;
-
 import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.ss.usermodel.Row;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import com.idega.block.media.business.ExcelUtil;
 import com.idega.core.business.DefaultSpringBean;
 
 import is.idega.idegaweb.egov.cases.media.ExcelExporterService;
@@ -18,36 +16,12 @@ public class ExcelExporterServiceImpl extends DefaultSpringBean implements Excel
 
 	@Override
 	public boolean autosizeSheetColumns(HSSFSheet sheet, int nrOfCells) {
-		if (sheet == null || nrOfCells <= 0) {
-			return false;
-		}
-
-		for (int column = 0; column < nrOfCells; column++) {
-			try {
-				sheet.autoSizeColumn(column);
-			} catch (Exception e) {}
-		}
-
-		return true;
+		return ExcelUtil.getInstance().autosizeSheetColumns(sheet, nrOfCells);
 	}
 
+	@Override
 	public boolean autosizeColumns(HSSFSheet sheet) {
-		if (sheet == null) {
-			return Boolean.FALSE;
-		}
-
-		short lastCell = 0;
-		for (Iterator<Row> rowIterator = sheet.iterator(); rowIterator.hasNext();) {
-			Row row = rowIterator.next();
-			if (row == null) {
-				continue;
-			}
-
-			short cellNum = row.getLastCellNum();
-			lastCell = lastCell < cellNum ? cellNum : lastCell;
-		}
-
-		return autosizeSheetColumns(sheet, lastCell);
+		return ExcelUtil.getInstance().autosizeColumns(sheet);
 	}
 
 }
